51d4aa89f0e3bf7a03d2cd87f4ec018d73c13d04
VAmanJain <vaman.jain17@gmail.com>
2023-09-05 15:37:35 +0530

    api correction

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-const API_KEY = "0068ed3fb26c4e769ab1d70da598aaef";
-const url = "https://newsapi.org/v2/everything?q=";
+
+
+const GNEWS_API_KEY = "5e03d15119b619681f013f719b916b30";
+const gnewsApiUrl = "https://gnews.io/api/v4/search";
 
 window.addEventListener("load", () => fetchNews("India"));
 
-function reload(){
-    window.location.reload();
+function reload() {
+  window.location.reload();
 }
 
 async function fetchNews(query) {
-  const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
-  const data = await res.json();
-  console.log(data);
-  bindData(data.articles);
+  try {
+    const res = await fetch(`${gnewsApiUrl}?q=${query}&token=${GNEWS_API_KEY}`);
+    if (!res.ok) {
+      throw new Error(`Error fetching news data from GNews API: ${res.status}`);
+    }
+    const data = await res.json();
+    console.log(data);
+    bindData(data.articles);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 function bindData(articles) {
@@ -20,8 +29,13 @@ function bindData(articles) {
 
   cardsContainer.innerHTML = "";
 
+  if (!articles || articles.length === 0) {
+    // FOR THE CASES WHEN NO NEWS ARTICLES ARE AVAILABLE
+    console.log("No news articles available.");
+    return;
+  }
+
   articles.forEach((article) => {
-    if (!article.urlToImage) return;
     const cardClone = newsCardTemplate.content.cloneNode(true);
     fillDataInCard(cardClone, article);
     cardsContainer.appendChild(cardClone);
@@ -34,39 +48,37 @@ function fillDataInCard(cardClone, article) {
   const newsSource = cardClone.querySelector("#news-source");
   const newsDesc = cardClone.querySelector("#news-desc");
 
-  newsImg.src = article.urlToImage;
-  newsTitle.innerHTML = article.title;
-  newsDesc.innerHTML = article.description;
+  newsImg.src = article.image || 'http://via.placeholder.com/400x200'; //USE IMAGE WHEN AVAILABLE OR USE PLACEHOLDER
 
-  const date = new Date(article.publishedAt).toLocaleString("en-US", {
-    timeZone: "Asia/Jakarta", // Corrected the timezone
-  });
-
-  newsSource.innerHTML = `${article.source.name} ▪️ ${date}`; // Fixed the variable name
+  newsTitle.textContent = article.title || "N/A";
+  newsDesc.textContent = article.description || "No description available";
+  newsSource.textContent = article.source || "Unknown source";
 
   cardClone.firstElementChild.addEventListener("click", () => {
-    window.open(article.url, "_blank");
+    if (article.url) {
+      window.open(article.url, "_blank");
+    }
   });
 }
 
-let curSelectedNav = null ;
-function onNavItemClick(id){
-    fetchNews(id);
-    const navItem = document.getElementById(id);
-    curSelectedNav?.classList.remove("active");
-    curSelectedNav = navItem;
-    curSelectedNav.classList.add("active");
+let curSelectedNav = null;
+function onNavItemClick(id) {
+  fetchNews(id);
+  const navItem = document.getElementById(id);
+  curSelectedNav?.classList.remove("active");
+  curSelectedNav = navItem;
+  curSelectedNav.classList.add("active");
 }
 
 const searchButton = document.getElementById("search-btn");
 const searchText = document.getElementById("search-text");
 
-searchButton.addEventListener("click" , () =>{
-    const query = searchText.value ; 
-    if(!query) return ;
+searchButton.addEventListener("click", () => {
+  const query = searchText.value;
+  if (!query) return;
 
-    fetchNews(query);
-    curSelectedNav?classList.remove("active"):
-    curSelectedNav =null;
-
+  fetchNews(query);
+  curSelectedNav?.classList.remove("active");
+  curSelectedNav = null;
 });
+
